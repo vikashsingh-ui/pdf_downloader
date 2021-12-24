00ae714d5f9914f839993f9249d4bd3c9b4940e7
vikash <vikashekma151299@gmail.com>
2021-12-24 10:59:00 +0530

    first commit

---
+++ index_instant.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>Download PDF using from HTML link </title>
+</head>
+<body>
+    <center>
+    <h2 style="color:#d7385e;">Welcome to vikash singh server</h2>
+    <p> <b>Click below to download PDF </b> </p>
+    <a href="download.php?file=ADTs" target="_vikash"> Download PDF NOW</a>
+    </center>
+
+
+</body>
+</html>
